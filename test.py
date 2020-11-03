a2da57fd6e1f5439f161ca89f505a1c566027dff
Bubbliiiing <47347516+bubbliiiing@users.noreply.github.com>
2020-11-03 15:12:49 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,12 +1,14 @@
+#--------------------------------------------#
+#   该部分代码只用于看网络结构，并非测试代码
+#   map测试请看get_dr_txt.py、get_gt_txt.py
+#   和get_map.py
+#--------------------------------------------#
 import torch
 from nets.retinanet import Retinanet
 from nets.retinanet import Resnet
 
 if __name__ == '__main__':
-
     inputs = torch.randn(5, 3, 512, 512)
-
-
     # Test inference
     model = Retinanet(80,2)
     print('# generator parameters:', sum(param.numel() for param in model.parameters()))
