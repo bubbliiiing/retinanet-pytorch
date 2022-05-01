76d33cbb8f78413678205e5815b51146d07c9de8
Bubbliiiing <3323290568@qq.com>
2022-05-01 14:12:19 +0800

    update flops and training steps

--- summary.py
+++ summary.py
@@ -1,9 +1,29 @@
 #--------------------------------------------#
 #   该部分代码用于看网络参数
 #--------------------------------------------#
+import torch
+from thop import clever_format, profile
+
 from nets.retinanet import retinanet
 
 if __name__ == '__main__':
-    model   = retinanet(80, 2)
+    input_shape     = [600, 600]
+    num_classes     = 80
+    phi             = 2
+    
+    device  = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model   = retinanet(num_classes, phi).to(device)
     print('# generator parameters:', sum(param.numel() for param in model.parameters()))
     
+    dummy_input     = torch.randn(1, 3, input_shape[0], input_shape[1]).to(device)
+    flops, params   = profile(model.to(device), (dummy_input, ), verbose=False)
+    #--------------------------------------------------------#
+    #   flops * 2是因为profile没有将卷积作为两个operations
+    #   有些论文将卷积算乘法、加法两个operations。此时乘2
+    #   有些论文只考虑乘法的运算次数，忽略加法。此时不乘2
+    #   本代码选择乘2，参考YOLOX。
+    #--------------------------------------------------------#
+    flops           = flops * 2
+    flops, params   = clever_format([flops, params], "%.3f")
+    print('Total GFLOPS: %s' % (flops))
+    print('Total params: %s' % (params))
